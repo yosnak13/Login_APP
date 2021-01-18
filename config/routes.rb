4281f14991a3e94c44b48fb8ef3c793529e445ce
yosnak13 <yosnak13@gmail.com>
2021-01-18 19:47:48 +0900

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # まだ設定をしていない場合は、いくつかの設定を手動で行う必要があります。
+  # routes.rb で生成されたコントローラのルートをオーバーライドしていることを確認してください。 例えば、以下のようにします。
+  #   Rails.application.routes.draw do
+  #     devise_for :users, controllers. {
+  #       sessions: 'users/sessions'
+  #     }
+  #   end
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlock => 'users/unclock',
+  }
+
+  devise_scope :user do
+    root :to => 'users/sessions#new'
+    get 'signup', :to => 'users/registrations#new'
+    get 'login', :to => 'users/sessions#new'
+    get 'logout', :to => 'users/sessions#destroy'
+  end
 end
