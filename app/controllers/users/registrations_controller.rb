3b773ae88c08a0c6d05ca9f5db60060de5cffb86
yosnak13 <yosnak13@gmail.com>
2021-01-19 13:52:46 +0900

    define route after createing an account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -31,6 +31,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  def verify
+  end
+
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
@@ -55,9 +58,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    veryfi_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
