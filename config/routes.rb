7f4931b82872470bd61bd8b8160a1cdd04b4b5e9
yosnak13 <yosnak13@gmail.com>
2021-01-20 13:03:54 +0900

    ""=>''

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     root :to => 'users/sessions#new'
     get 'signup', :to => 'users/registrations#new'
-    get "verify", :to => "users/registrations#verify"
+    get 'verify', :to => 'users/registrations#verify'
     get 'login', :to => 'users/sessions#new'
     get 'logout', :to => 'users/sessions#destroy'
   end
