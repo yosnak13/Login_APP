2a2e3d36cad914302b3194c9b99f22b64c6dc723
yosnak13 <yosnak13@gmail.com>
2021-01-20 14:21:52 +0900

    define logout method and edit view for logout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -14,9 +14,9 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   def after_sign_in_path_for(user)
     index_path
